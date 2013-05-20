13ec8bad0f9acb350f7c6b07bfcad9a67da00ae4
Naor <naor@mediabox.io>
2013-05-21 01:22:02 +0300

    - Remove bullet swing

--- public/app/modules/account/views/login.js
+++ public/app/modules/account/views/login.js
@@ -8,17 +8,13 @@ define([
         bulletPadding = 15,
         bulletMargin = 15,
         bulletsPadding = 15,
-        highlightDuration = 1500, //ms
-        smallScreenDivider = 1.5,
-        maxBulletSwing = 30; //deg
+        smallScreenDivider = 1.5;
 
     var LoginView = Backbone.View.extend({
         className: 'login-view',
         template: 'account/login',
         events: {
-            'click .bullet': 'highlightLogin'//,
-            //'mouseleave .bullet-wrapper': 'mouseLeaveBullet',
-            //'mousemove .bullet-wrapper': 'mouseMoveOnBullet'
+            'click .bullet': 'highlightLogin'
         },
         initialize: function() {
             this.calculateBulletsWidth =
@@ -58,39 +54,6 @@ define([
             $('html, body')
                 .animate({ scrollTop: top }, 400, 'swing')
                 .promise().done(blink);
-        },
-        mouseLeaveBullet: function(event) {
-            var bullet = $(event.currentTarget).find('.bullet');
-            this.clearBulletRotate(bullet);
-        },
-        mouseMoveOnBullet: function(event) {
-            var bullet = $(event.currentTarget).find('.bullet'),
-                left = bullet.position().left + bulletMargin,
-                width = bullet.outerWidth(),
-                center = width / 2,
-                curRelPosX = event.clientX - left,
-                percent = curRelPosX <= center ?
-                    curRelPosX / center :
-                    (width - curRelPosX) / center,
-                rotatePosition = curRelPosX <= center ? 1 : -1,
-                rotateY = rotatePosition * maxBulletSwing * (1 - percent);
-
-            this.rotateBullet(bullet, rotateY);
-        },
-        clearBulletRotate: function(bullet) {
-            bullet.css({
-                'transform': 'rotateY(0deg)',
-                'box-shadow': ''
-            });
-        },
-        rotateBullet: function(bullet, rotateY) {
-            var boxShadow =
-                rotateY * 0.2 + 'px 0 20px 0px black';
-
-            bullet.css({
-                'transform': 'rotateY(' + rotateY + 'deg)',
-                'box-shadow': '0px 0 20px 0px black'
-            });
         }
     });
 
